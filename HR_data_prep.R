3f97744799ae5f3c1243187d86b1374a94fffaa4
AndersonHoff <ande_hoff@yahoo.com.br>
2020-05-14 21:19:01 -0400

    Data preparation

--- HR_data_prep.R
+++ HR_data_prep.R
@@ -1,17 +1,28 @@
-if(!require(dplyr)) {install.packages("dplyr")} else {library(dplyr)}
+#if(!require(dplyr)) {install.packages("dplyr")} else {library(dplyr)}
 
 ###### PREPARING DATA #######################
 
 traindata=read.csv("train_data.csv",stringsAsFactors = F,header = T)
 str(traindata)
+head(traindata)
+glimpse(traindata)
 
 traindata <- traindata %>% 
   select(is_promoted, everything())
+
+colnames(traindata) <- c("Is_Promoted", "Employee_ID", "Department", "Region", "Education", 
+                         "Gender", "Recruitment_Channel", "Number_of_Trainings", "Age", 
+                         "Previous_Year_Rating", "Length_of_Service", "KPIs_met_80", "Awards_Won", 
+                         "Average_Training_Score")
 
 for(i in 1:14){
   if(class(traindata[,i])=="character")
   {traindata[,i]=as.factor(traindata[,i]) }
 }
+
+traindata$Is_Promoted <- as.factor(traindata$Is_Promoted)
+traindata$KPIs_met_80 <- as.factor(traindata$KPIs_met_80)
+traindata$Awards_Won <- as.factor(traindata$Awards_Won)
 
 for(i in 1:14){
   if(class(traindata[,i])=="integer")
@@ -21,37 +32,34 @@ for(i in 1:14){
 rm(i)
 ####Ver quantidade de NAs na coluna
 
-sum(is.na(traindata$no_of_trainings))
-sum(is.na(traindata$previous_year_rating))
-sum(is.na(traindata$age))
-sum(is.na(traindata$length_of_service))
-sum(is.na(traindata$KPIs_met..80.))
-sum(is.na(traindata$awards_won.))
-sum(is.na(traindata$avg_training_score))
+sum(is.na(traindata$Number_of_Trainings))
+sum(is.na(traindata$Previous_Year_Rating))
+sum(is.na(traindata$Age))
+sum(is.na(traindata$Length_of_Service))
+sum(is.na(traindata$KPIs_met_80))
+sum(is.na(traindata$Awards_Won))
+sum(is.na(traindata$Average_Training_Score))
 
-levels(traindata$department)
-levels(traindata$region)
-levels(traindata$education)
-levels(traindata$gender)
-levels(traindata$recruitment_channel)
+levels(traindata$Department)
+levels(traindata$Region)
+levels(traindata$Education)
+levels(traindata$Gender)
+levels(traindata$Recruitment_Channel)
+levels(traindata$KPIs_met_80)
+levels(traindata$Awards_Won)
 
 ####### Insere 0 para NA na coluna "previous_year_rating"
 
-traindata$previous_year_rating[is.na(traindata$previous_year_rating)] <- 0
+traindata$Previous_Year_Rating[is.na(traindata$Previous_Year_Rating)] <- 0
 
 ##Inserir None em education (missing values)
 
-levels(traindata$education)
-traindata$education = factor(traindata$education,levels = c(levels(traindata$education),"None"))
-levels(traindata$education)[1]
-levels(traindata$education)[1] <- NA
-traindata$education[is.na(traindata$education)] <- "None"
+levels(traindata$Education)
+traindata$Education = factor(traindata$Education,levels = c(levels(traindata$Education),"None"))
+levels(traindata$Education)[1]
+levels(traindata$Education)[1] <- NA
+traindata$Education[is.na(traindata$Education)] <- "None"
 
 ######### Save prepared data 
-
-colnames(traindata) <- c("Is Promoted", "Employee ID", "Department", "Region", "Education", 
-               "Gender", "Recruitment Channel", "Number of Trainings", "Age", 
-                "Previous Year Rating", "Length of Service", "KPIs met .80", "Awards Won", 
-                "Average Training Score")
 
 write.csv(traindata, file="HR_traindata.csv", quote = FALSE, row.names=F)
